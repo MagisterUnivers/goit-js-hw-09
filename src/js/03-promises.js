4d2c299f1a72c2236eb577bd0b8545a84e1c98a7
Andrey Shaposhnikov <droniiiandruxa@gmail.com>
2023-03-23 23:51:46 +0200

    HW9 finish

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -28,11 +28,13 @@ refs.form.addEventListener('submit', e => {
 
     createPromise(index, totalStep)
       .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
         Notiflix.Notify.success(
           `✅ Fulfilled promise ${position} in ${delay}ms`
         );
       })
       .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
         Notiflix.Notify.failure(
           `❌ Rejected promise ${position} in ${delay}ms`
         );
